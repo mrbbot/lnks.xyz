95f19052360393b6e072bcb66dea59f961ea0ad9
MrBBot <me@mrbbot.dev>
2019-12-23 16:24:54 +0000

    Adapt for Dokku deployment

--- db/redis.go
+++ db/redis.go
@@ -2,21 +2,38 @@ package db
 
 import (
 	"github.com/go-redis/redis"
+	"net/url"
 	"os"
 	"strconv"
 )
 
 var R *redis.Client
 
+func getOptions() (*redis.Options, error) {
+	redisUrl, err := url.Parse(os.Getenv("REDIS_URL"))
+	if err != nil {
+		return nil, err
+	}
+	redisUrlPassword, ok := redisUrl.User.Password()
+	redisPassword := ""
+	if ok {
+		redisPassword = redisUrlPassword
+	}
+	redisDb, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		return nil, err
+	}
+	return &redis.Options{
+		Addr:     redisUrl.Host,
+		Password: redisPassword,
+		DB:       redisDb,
+	}, nil
+}
+
 func init() {
-	db, err := strconv.Atoi(os.Getenv("S_REDIS_DB"))
+	opt, err := getOptions()
 	if err != nil {
 		panic(err)
 	}
-
-	R = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("S_REDIS_ADDRESS"),
-		Password: os.Getenv("S_REDIS_PASSWORD"),
-		DB:       db,
-	})
+	R = redis.NewClient(opt)
 }
